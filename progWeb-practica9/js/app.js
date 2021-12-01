6532c7bcd751d4c3177eba6fa01758242e018842
JuanjoLaptop <juanjo.silva.098@gmail.com>
2021-12-01 16:50:48 -0600

    01/12/2021

--- progWeb-practica9/js/app.js
+++ progWeb-practica9/js/app.js
@@ -3,15 +3,104 @@ By: Juan José Silva López*/
 
 // Declarar variables
 const listaProductos = document.querySelector('.lista-productos');
+const miCarrito = document.querySelector('#mi-carrito');
+const numeroProductos = document.getElementById('numero-producto');
+
+let carrito = [];
 
 cargarEventListeners();
 
 function cargarEventListeners() {
-    listaProductos.addEventListener("click", cargarProducto)
+    listaProductos.addEventListener("click", cargarProducto);
+    document.addEventListener('DOMContentLoaded', () =>{
+        carrito = JSON.parse(localStorage.getItem('carrito')) || 0;
+        dibujarCarrito();
+    })
 };
 
+function obtenerInformacionProducto(producto) {
+    infoProducto = {
+        imagen: producto.children[0].style.backgroundImage.replace(/"/g, ''),
+        nombre: producto.querySelector('.info-producto h2').textContent,
+        precio: producto.querySelector('.info-producto .precio-producto').textContent,
+        cantidad: 1,
+        id: producto.querySelector('a').getAttribute('data-id')
+    }
+    // Verificar si el producto ya existe en el carrito
+    const existe = carrito.some(producto => producto.id === infoProducto.id);
 
-function cargarProducto(e){
-    if (e.target.classList.contains('agregar-carrito') || e.target.parentNode.classList.contains('agregar-carrito'))
-        console.log("Agregar a carrito");
+    if (existe) {
+        const copiaCarrito = carrito.map(prod => {
+            if (prod.id == infoProducto.id)
+                prod.cantidad += 1;
+            return prod;
+        });
+        carrito = [...copiaCarrito];
+    }
+    else
+        carrito = [...carrito, infoProducto];
+}
+
+function cargarProducto(e) {
+    e.preventDefault();
+    if (e.target.classList.contains('agregar-carrito')) {
+        const producto = e.target.parentElement.parentElement.parentElement.parentElement;
+        obtenerInformacionProducto(producto);
+        dibujarCarrito();
+        sincronizarLocalStorage();
+    }
+    if (e.target.classList.contains('flaticon-shopping-bag')) {
+        const producto = e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
+        obtenerInformacionProducto(producto);
+        dibujarCarrito();
+        sincronizarLocalStorage();
+    }
+};
+
+// Dibuja los productos del carrito en la parte superior
+function dibujarCarrito() {
+    limpiarCarritoHTML();
+    carrito.forEach(producto => {
+        const divHTML = document.createElement('div');
+        divHTML.innerHTML = `<div class="dropdown-item d-flex align-items-start" href="#">
+                                <div class="img" style="background-image: ${producto.imagen};"></div>
+                                <div class="text pl-3">
+                                    <h4>${producto.nombre}</h4>
+                                    <p class="mb-0"><a href="#" class="price">${producto.precio}</a><span class="quantity ml-3">Quantity: ${producto.cantidad}</span></p>
+                                </div>
+                            </div>`;
+        miCarrito.appendChild(divHTML);
+    });
+    miCarrito.appendChild(crearBrnVerCarrito());
+    actualizarNumero();
+}
+
+// Limpia el contenido del carrito para volver a pintarlo
+function limpiarCarritoHTML() {
+    while (miCarrito.firstChild) {
+        miCarrito.removeChild(miCarrito.firstChild);
+    }
+    // miCarrito.innerHTML = '';
+}
+
+function crearBrnVerCarrito() {
+    const elementoA = document.createElement('a');
+    elementoA.setAttribute("class", "dropdown-item text-center btn-link d-block w-100")
+    elementoA.setAttribute("href", "cart.html")
+    elementoA.text = "Ver el contenido del carito"
+
+    const elementoSPAN = document.createElement('span');
+    elementoSPAN.setAttribute("class", "ion-ios-arrow-round-forward")
+    elementoA.appendChild(elementoSPAN);
+
+    return elementoA;
+}
+
+// Función que actualiza el número de productos
+function actualizarNumero() {
+    numeroProductos.textContent = carrito.length;
+}
+
+function sincronizarLocalStorage() {
+    localStorage.setItem('carrito', JSON.stringify(carrito))
 }
